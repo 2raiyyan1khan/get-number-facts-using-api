0eb92511cb81f14d4cfbfd90b1dcde9cad205ce9
2raiyyan1khan <inforaiyyan@gmail.com>
2021-09-24 11:01:13 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let switcher = document.getElementById("switcher");
+const form = document.getElementById("form");
+const number = document.getElementById("number");
+const factBox = document.getElementById("facts");
+
+form.addEventListener("submit", getNumber);
+
+async function getNumber(e) {
+  e.preventDefault();
+  const fact = await fetch(`http://numbersapi.com/${parseInt(number.value)}`, {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => data)
+    .catch((err) => console.log(err));
+  if (fact) {
+    factBox.style.display = "block";
+    factBox.innerHTML = `<p>${fact}</p>`;
+  }
+  number.value = "";
+}
+
+switcher.addEventListener("click", () => {
+  if (document.body.dataset.theme === "dark") {
+    document.body.dataset.theme = "light";
+    switcher.innerHTML = '<i class="far fa-moon"></i>';
+  } else {
+    document.body.dataset.theme = "dark";
+    switcher.innerHTML = '<i class="far fa-sun"></i>';
+  }
+});
